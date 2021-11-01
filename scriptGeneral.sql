44d17efa8d3b69f34193db64fd5c12a9329c5aa4
Alberto Rosado <albrosesc@gmail.com>
2021-11-01 13:50:15 -0600

    version 1

--- scriptGeneral.sql
+++ scriptGeneral.sql
@@ -1,100 +1,45 @@
-DROP DATABASE if exists `um`;
-CREATE DATABASE `um`
+DROP DATABASE if exists `sql_learning`;
+CREATE DATABASE `sql_learning`
     DEFAULT CHARACTER SET = latin1
 	DEFAULT COLLATE = latin1_spanish_ci;
 	
-USE `um`;
+USE `sql_learning`;
 
-  DROP TABLE IF EXISTS `tbniveles`;
-  CREATE TABLE `tbniveles` (
-  `idNivel` tinyint(1) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(45) NOT NULL,
-  PRIMARY KEY (`idNivel`)
-) ENGINE=InnoDB  COMMENT='Catalogo de Niveles de Colocación';
-
-INSERT INTO `um`.`tbniveles` (`descripcion`) VALUES ('Básico');
-INSERT INTO `um`.`tbniveles` (`descripcion`) VALUES ('Medio'); 
-INSERT INTO `um`.`tbniveles` (`descripcion`) VALUES ('Alto'); 
-INSERT INTO `um`.`tbniveles` (`descripcion`) VALUES ('Nativo');
+  DROP TABLE IF EXISTS `oph`;
+  CREATE TABLE `oph` (
+  `NoOrder` int NOT NULL AUTO_INCREMENT,
+  `DateSupplier` Datetime NOT NULL,
+  PRIMARY KEY (`NoOrder`)
+) ENGINE=InnoDB  COMMENT='Catalogo de Order Purchase Header';
+show databases;
+INSERT INTO `sql_learning`.`oph` (`DateSupplier`) VALUES (now());
+INSERT INTO `sql_learning`.`oph` (`DateSupplier`) VALUES  (ADDDATE(now(), INTERVAL 7 DAY)); 
+INSERT INTO `sql_learning`.`oph` (`DateSupplier`) VALUES  (ADDDATE(now(), INTERVAL 15 DAY)); 
+INSERT INTO `sql_learning`.`oph` (`DateSupplier`) VALUES  (ADDDATE(now(), INTERVAL 30 DAY));
 	
-DROP TABLE if exists `tbcarreras`;
+DROP TABLE if exists `opl`;
 
-CREATE TABLE `tbcarreras` (
-  `idCarrera` tinyint(1) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
-  `descripcion` varchar(38)  NOT NULL COMMENT 'Nombre de las diferentes carreras de la Universidad Marista.',
-  PRIMARY KEY (`idCarrera`),
-  UNIQUE KEY `descripcion_UNIQUE` (`descripcion`)
-) ENGINE=InnoDB COMMENT='Cátalogo con las carreras de la Universidad Marista';
+CREATE TABLE `opl` (
+  `idLine` tinyint(1) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `idNoProducto` varchar(1)  NOT NULL COMMENT 'Número de Producto',
+  `ophNoOrder` int, 
+  INDEX Norder(idline, ophNoOrder),
+  PRIMARY KEY (idLine),
+  FOREIGN KEY (ophNoOrder) REFERENCES oph(NoOrder)
+  ) ENGINE=InnoDB COMMENT='Detalle de la ORDER Purchase';
 
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Actuaria');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Administracion y Direccion del Deporte');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Administracion y Mercadotecnia');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Arquitectura');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Ciencias de la Comunicacion');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Contaduria');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Derecho');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Diseno Grafico');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Ingeniería Industrial');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Ingeniería Mecatrónica');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Negocios Internacionales');
-INSERT INTO `um`.`tbcarreras` (`descripcion`) VALUES ('Psicologia');
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('A', 1);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('B', 1);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('C', 1);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('B', 2);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('D', 2);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('C', 3);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('A', 4);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('B', 4);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('C', 4);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('D', 4);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('E', 4);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('E', 1);
+INSERT INTO `sql_learning`.`opl` (`idNoProducto`,`ophNoOrder`) VALUES ('C', 2);
 
 
-DROP TABLE if exists `tbpreguntas`;
-
-CREATE TABLE `tbpreguntas` (
-  `idPregunta` int(11) NOT NULL AUTO_INCREMENT,
-  `oracion` varchar(200) NOT NULL,
-  `idNivel` tinyint(1) NOT NULL DEFAULT '1',  
-  `o1` varchar(50) NOT NULL,
-  `v1` tinyint(1) NOT NULL CHECK (v1 IN (0,1)),
-  `o2` varchar(50) NOT NULL,
-  `v2` tinyint(1) NOT NULL CHECK (v2 IN (0,1)),
-  `o3` varchar(50) NOT NULL,
-  `v3` tinyint(1) NOT NULL CHECK (v3 IN (0,1)),
-  `o4` varchar(50) DEFAULT NULL,
-  `v4` tinyint(1) NOT NULL CHECK (v4 IN (0,1)),  
-  `imagen` blob  DEFAULT NULL,
-  PRIMARY KEY (`idPregunta`),
-  KEY `fkNivelPregunta_idx` (`idNivel`),
-  CONSTRAINT `fkNivelPregunta` FOREIGN KEY (`idNivel`) REFERENCES `tbniveles` (`idNivel`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB  COMMENT='En esta tabla se almacenarán las preguntas del examen de colocación para medir el nivel de inglés de un alumno en la Universidad marista.';
-
-/*DROP VIEW if exists vPreguntas;
-CREATE VIEW vPreguntas AS 
-  SELECT * FROM tbpreguntas 
-  WHERE nivel  IN (1, 2, 3)
-  AND v1 IN (0,1)
-  AND v2 IN (0,1)
-  AND v3 IN (0,1)
-  AND v4 IN (0,1)
-  AND v5 IN (0,1)
-  WITH CHECK OPTION;*/ /*'Esta vista se crea para garantizar la integridad relacional de la tbPreguntas.'*/
-  
-
-DROP TABLE if exists `tbaspirantes`;
-CREATE TABLE `tbaspirantes` (
-  `idAspirante` int(11) NOT NULL AUTO_INCREMENT,
-  `nombres` varchar(120) NOT NULL COMMENT 'Nombre o nombres del Aspirante. Mandatory',
-  `aPaterno` varchar(80) NOT NULL COMMENT 'Apellido Paterno del Aspirante. Mandatory',
-  `aMaterno` varchar(80) DEFAULT NULL COMMENT 'Apellido Materno del aspirante. NOT Mandatory',
-  `fNacimiento` date NOT NULL COMMENT 'Fecha de nacimiento del aspirante. Mandatory',
-  `escuelaProc` varchar(250) NOT NULL COMMENT 'Escuela de Procedencia. Mandatory',
-  `tipoEscuela` tinyint(1) DEFAULT NULL COMMENT 'Clasifica el tipo de Escuela donde procede el estudiante',
-  `idNivel` tinyint(1) NOT NULL DEFAULT '1',
-  `idCarrera` tinyint(1) NOT NULL COMMENT 'Carrera en la que se encuentra inscrito el aspirante',
-  PRIMARY KEY (`idAspirante`),  
-  KEY `fkAspiranteCarrera_idx` (`idCarrera`),
-  CONSTRAINT `fkAspiranteCarrera` FOREIGN KEY (`idCarrera`) REFERENCES `tbcarreras` (`idCarrera`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB  COMMENT='tabla que registra los datos de los aspirantes.';
-
-DROP TABLE if exists `tbaspirantesniveles`;
-CREATE TABLE `tbaspirantesniveles` (
-  `idAspirante` int(11) NOT NULL,
-  `idPregunta` int(11) NOT NULL,
-  `respuesta` tinyint(1) DEFAULT NULL,
-  KEY `fkAspirantePregunta_idx` (`idPregunta`),
-  KEY `fkAspirante_idx` (`idAspirante`),
-  CONSTRAINT `fkAspirantePregunta` FOREIGN KEY (`idPregunta`) REFERENCES `tbpreguntas` (`idPregunta`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fkAspirante` FOREIGN KEY (`idAspirante`) REFERENCES `tbaspirantes` (`idAspirante`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB;
